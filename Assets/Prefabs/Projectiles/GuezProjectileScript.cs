a49c768526e8b24b1999de8ec941b20d95088c4a
zap174 <antonledragonrouge@gmail.com>
2023-08-22 18:57:42 +0200

    LES ZIZIS
    J AI MIS UN ZIZI  DANS LE JEU VIDEO

--- Assets/Prefabs/Projectiles/GuezProjectileScript.cs
+++ Assets/Prefabs/Projectiles/GuezProjectileScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseProjectileScript : MonoBehaviour
+public class GuezProjectileScript : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float bulletSpeed;
